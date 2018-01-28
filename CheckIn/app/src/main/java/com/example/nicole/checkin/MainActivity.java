a2ad597761c3e20eda94ddc4c93cb9ba6c12ee42
Nicole Fella <nicolelfella@gmail.com>
2018-01-28 00:38:12 -0500

    Reorder activities and fix button

--- CheckIn/app/src/main/java/com/example/nicole/checkin/MainActivity.java
+++ CheckIn/app/src/main/java/com/example/nicole/checkin/MainActivity.java
@@ -4,15 +4,27 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnChangePages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnChangePages = (Button) findViewById(R.id.btn_checkin);
+
+        btnChangePages.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                launchActivity();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -22,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void launchActivity(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 
 }
